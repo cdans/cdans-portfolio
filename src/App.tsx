06caf711f9c78d6ec7844467a9c83dd7e0dfa671
cdans <ch.dansard@gmail.com>
2025-02-09 12:06:33 +0100

    add jump capability to mario

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,36 @@ import "./App.css";
 
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { useEffect, useRef } from "react";
 
 import { MainPage } from "./pages/MainPage";
 
 export const App = () => {
+  const marioRef = useRef<HTMLDivElement>(null);
+  const isJumping = useRef(false);
+
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.code === "Space" && !isJumping.current && marioRef.current) {
+        event.preventDefault();
+        isJumping.current = true;
+        marioRef.current.classList.add("jump");
+
+        setTimeout(() => {
+          marioRef.current?.classList.remove("jump");
+          isJumping.current = false;
+        }, 500);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, []);
+
   return (
     <>
       <div className="background"></div>
+      <div className="mario" ref={marioRef}></div>
       <Analytics />
       <SpeedInsights />
       <MainPage />
